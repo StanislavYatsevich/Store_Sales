9bd51886ca1aca8a41a686ebe9628d18eaca9493
Stas <Stas20042701@gmail.com@>
2024-06-18 18:39:30 +0300

    Completed click interface

--- src/store_sales/adding_features.py
+++ src/store_sales/adding_features.py
@@ -1,10 +1,19 @@
 import pandas as pd
 from store_sales import add_features
+import click
 
-train_data = pd.read_csv("../../data/prepared_data/train_data.csv")
-valid_data = pd.read_csv("../../data/prepared_data/valid_data.csv")
-test_data = pd.read_csv("../../data/prepared_data/test_data.csv")
+@click.command()
+@click.option('--input_data_folder', required=True, type=click.Path(exists=True), help='Path to the folder with input data files')
+@click.option('--output_data_folder', required=True, type=click.Path(), help='Path to the folder where processed files are saved to')
 
-data = pd.concat([train_data, valid_data, test_data], axis=0)
-data = add_features(data)
-data.to_csv("../../data/prepared_data/prepared_data.csv", index=False)
+def adding_features(input_data_folder, output_data_folder):
+    train_data = pd.read_csv(f'{input_data_folder}/train_data.csv')
+    valid_data = pd.read_csv(f'{input_data_folder}/valid_data.csv')
+    test_data = pd.read_csv(f'{input_data_folder}/test_data.csv')
+
+    data = pd.concat([train_data, valid_data, test_data], axis=0)
+    data = add_features(data)
+    data.to_csv(f'{output_data_folder}/prepared_data.csv', index=False)
+
+if __name__ == '__main__':
+    adding_features()
--- src/store_sales/split_and_prepare.py
+++ src/store_sales/split_and_prepare.py
@@ -1,38 +1,35 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from store_sales import prepare_data
+import click
 
-#Click options/command here
 
-"""@click.command()
-@click.option('--input_path', required=True, type=click.Path(exists=True), help='Path to the input file')
-@click.option('--output_path', required=True, type=click.Path(), help='Path to save the processed file')
+@click.command()
+@click.option('--raw_data_folder', required=True, type=click.Path(exists=True), help='Path to the folder with raw data files')
+@click.option('--prepared_data_folder', required=True, type=click.Path(), help='Path to the folder where processed files are saved to')
 
-def preprocess(input_path, output_path):
-    preprocess_data(input_path, output_path)
+def split_and_prepare_data(raw_data_folder, prepared_data_folder):
+    data = pd.read_csv(f'{raw_data_folder}/train.csv')
+    holidays_events_data = pd.read_csv(f'{raw_data_folder}/holidays_events.csv')
+    oil_data = pd.read_csv(f'{raw_data_folder}/oil.csv')
+    stores_data = pd.read_csv(f'{raw_data_folder}/stores.csv')
 
-    if __name__ == '__main__':
-        preprocess()
-"""
+    X = data.drop(['sales'], axis=1)
+    y = data['sales']
+    X_train, X_valid_test, y_train, y_valid_test = train_test_split(X, y, test_size=0.2, shuffle=False, random_state=42)
+    X_valid, X_test, y_valid, y_test = train_test_split(X_valid_test, y_valid_test, shuffle=False, test_size=0.5, random_state=42)
 
-data = pd.read_csv('../../data/raw_data/train.csv')
-holidays_events_data = pd.read_csv('../../data/raw_data/holidays_events.csv')
-oil_data = pd.read_csv('../../data/raw_data/oil.csv')
-stores_data = pd.read_csv('../../data/raw_data/stores.csv')
+    train_data = pd.concat([X_train, y_train], axis=1)
+    valid_data = pd.concat([X_valid, y_valid], axis=1)
+    test_data = pd.concat([X_test, y_test], axis=1)
 
-X = data.drop(['sales'], axis=1)
-y = data['sales']
-X_train, X_valid_test, y_train, y_valid_test = train_test_split(X, y, test_size=0.2, shuffle=False, random_state=42)
-X_valid, X_test, y_valid, y_test = train_test_split(X_valid_test, y_valid_test, shuffle=False, test_size=0.5, random_state=42)
+    train_data = prepare_data(train_data, holidays_events_data, oil_data, stores_data)
+    valid_data = prepare_data(valid_data, holidays_events_data, oil_data, stores_data)
+    test_data = prepare_data(test_data, holidays_events_data, oil_data, stores_data)
 
-train_data = pd.concat([X_train, y_train], axis=1)
-valid_data = pd.concat([X_valid, y_valid], axis=1)
-test_data = pd.concat([X_test, y_test], axis=1)
+    train_data.to_csv(f'{prepared_data_folder}/train_data.csv', index=False)
+    valid_data.to_csv(f'{prepared_data_folder}/valid_data.csv', index=False)
+    test_data.to_csv(f'{prepared_data_folder}/test_data.csv', index=False)
 
-train_data = prepare_data(train_data, holidays_events_data, oil_data, stores_data)
-valid_data = prepare_data(valid_data, holidays_events_data, oil_data, stores_data)
-test_data = prepare_data(test_data, holidays_events_data, oil_data, stores_data)
-
-train_data.to_csv("../../data/prepared_data/train_data.csv", index=False)
-valid_data.to_csv("../../data/prepared_data/valid_data.csv", index=False)
-test_data.to_csv("../../data/prepared_data/test_data.csv", index=False)
+if __name__ == '__main__':
+    split_and_prepare_data()
