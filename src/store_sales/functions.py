f66cc6afb91231ff1dcdb273e0caf1e4ef741047
Stas <Stas20042701@gmail.com@>
2024-08-14 14:54:54 +0300

    Refactor old function get_metrics_cross_validation()to get_models_and_metrics_cross_validation()

--- src/store_sales/functions.py
+++ src/store_sales/functions.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error
-from sklearn.base import RegressorMixin
+from sklearn.base import RegressorMixin, clone
 from typing import List, Any, Tuple, Union
 from sklearn.model_selection import TimeSeriesSplit
 import optuna
@@ -221,19 +221,22 @@ def get_mae(
 ) -> float:
     model.fit(X_train, y_train)
     y_pred = pd.Series(model.predict(X_test), index=y_test.index)
-    mae_score = np.round(mean_absolute_error(y_test, y_pred), 2)
+    mae_score = mean_absolute_error(y_test, y_pred)
     return mae_score
 
 
-def get_metrics_cross_validation(
+def get_models_and_metrics_cross_validation(
     train_data: pd.DataFrame, model: RegressorMixin
 ) -> Tuple[float, float, float]:
     tscv = TimeSeriesSplit(n_splits=5)
-    mae_scores = []
-    avg_sales = []
+    mae_scores = dict()
+    avg_sales = dict()
+    wmape_percentage_scores = dict()
+    models = dict()
 
     for store_num in train_data["store_number"].unique():
         for item_family in train_data["item_family"].unique():
+            mae_scores_this_split = []
             data = train_data[
                 (train_data["store_number"] == store_num)
                 & (train_data["item_family"] == item_family)
@@ -246,14 +249,23 @@ def get_metrics_cross_validation(
                 X_train_encoded, X_test_encoded = encode_features(
                     X_train.copy(), X_test.copy()
                 )
-                mae = get_mae(X_train_encoded, X_test_encoded, y_train, y_test, model)
-                avg_sales.append(data["item_sales"].mean())
-                mae_scores.append(mae)
+                model_clone = clone(model)
+                model_clone.fit(X_train_encoded, y_train)
+                y_pred = pd.Series(model_clone.predict(X_test_encoded), index=y_test.index)
+                mae = mean_absolute_error(y_test, y_pred)
+                mae_scores_this_split.append(mae)
 
-    mae = np.round(np.array(mae_scores).mean(), 2)
-    mean_sales = np.round(np.array(avg_sales).mean(), 2)
-    wmape_in_percentage = np.round(mae / mean_sales * 100, 2)
-    return mae, mean_sales, wmape_in_percentage
+            epsilon = 1**(-10)
+            avg_sales_this_series = np.round(data["item_sales"].mean(), 2)
+            mae_this_series = np.round(np.array(mae_scores_this_split).mean(), 2)
+            wmape_percentage_this_series = np.round(100 * mae_this_series / (avg_sales_this_series + epsilon), 2)
+
+            avg_sales[(store_num, item_family)] = avg_sales_this_series
+            mae_scores[(store_num, item_family)] = mae_this_series
+            wmape_percentage_scores[(store_num, item_family)] = wmape_percentage_this_series
+            models[(store_num, item_family)] = model_clone
+
+    return mae_scores, avg_sales, wmape_percentage_scores, models
 
 
 def optimize_xgboost_params_with_optuna(train_data, shops_number, n_trials):
