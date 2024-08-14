56cabeaaf0a810e426b28210c8bdf4eb5927e99f
Stas <Stas20042701@gmail.com@>
2024-08-14 14:52:03 +0300

    Add 2 new constants that represent default path values for saving_metrics_and_models.py script

--- src/store_sales/__init__.py
+++ src/store_sales/__init__.py
@@ -2,26 +2,36 @@ from .functions import (
     prepare_data,
     encode_features,
     get_tree_based_predicts,
-    add_features,
     get_mae,
-    get_metrics_cross_validation,
+    add_features,
+    get_models_and_metrics_cross_validation,
+    optimize_xgboost_params_with_optuna,
 )
 from .constants import (
     RAW_DATA_FOLDER_PATH,
     PREPARED_DATA_STAGE_1_FOLDER_PATH,
     PREPARED_DATA_STAGE_2_FOLDER_PATH,
     DATA_FOR_STREAMLIT_PATH,
+    METRICS_FOLDER_PATH,
+    MODELS_FOLDER_PATH,
 )
+from .mlflow_utils import init_mlflow_experiment, log_params_metrics_and_tags, log_model
 
 __all__ = [
     "prepare_data",
     "encode_features",
     "get_tree_based_predicts",
-    "add_features",
     "get_mae",
-    "get_metrics_cross_validation",
+    "add_features",
+    "get_models_and_metrics_cross_validation",
+    "optimize_xgboost_params_with_optuna",
     "RAW_DATA_FOLDER_PATH",
     "PREPARED_DATA_STAGE_1_FOLDER_PATH",
     "PREPARED_DATA_STAGE_2_FOLDER_PATH",
+    "METRICS_FOLDER_PATH",
+    "MODELS_FOLDER_PATH",
     "DATA_FOR_STREAMLIT_PATH",
+    "init_mlflow_experiment",
+    "log_params_metrics_and_tags",
+    "log_model",
 ]
--- src/store_sales/constants.py
+++ src/store_sales/constants.py
@@ -6,3 +6,6 @@ DATA_FOR_STREAMLIT_PATH = Path("../../data/prepared_data/train_data.csv")
 RAW_DATA_FOLDER_PATH = Path("../../data/raw_data")
 PREPARED_DATA_STAGE_1_FOLDER_PATH = Path("../../data/prepared_data")
 PREPARED_DATA_STAGE_2_FOLDER_PATH = Path("../../data/prepared_data")
+
+METRICS_FOLDER_PATH = Path("../../data/saved_metrics_and_models")
+MODELS_FOLDER_PATH = Path("../../data/saved_metrics_and_models")
