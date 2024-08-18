2a69956325fb354a9067db5af6c03ed30ebba5f6
Stas <Stas20042701@gmail.com@>
2024-08-18 19:20:23 +0300

    Format with Black

--- src/store_sales/adding_features.py
+++ src/store_sales/adding_features.py
@@ -1,11 +1,11 @@
 import pandas as pd
+import click
+from pathlib import Path
 from store_sales import (
     add_features,
     PREPARED_DATA_STAGE_1_FOLDER_PATH,
     PREPARED_DATA_STAGE_2_FOLDER_PATH,
 )
-from pathlib import Path
-import click
 
 
 @click.command()
