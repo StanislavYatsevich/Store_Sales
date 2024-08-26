1848e5f5f42b38292730adb1d77ead526d1ae670
Stas <Stas20042701@gmail.com@>
2024-08-26 18:06:13 +0300

    Refactor functions according to new constants from constants.py

---
+++ src/store_sales/adding_new_features.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import click
+from pathlib import Path
+from store_sales import (
+    add_features,
+    PREPARED_FOR_EDA_DATA_FOLDER_PATH,
+    PREPARED_FINAL_DATA_FOLDER_PATH,
+)
+
+
+@click.command()
+@click.option(
+    "--input_data_folder_path",
+    default=PREPARED_FOR_EDA_DATA_FOLDER_PATH,
+    type=click.Path(exists=True),
+    help="Path to the folder with input data files",
+)
+@click.option(
+    "--prepared_final_data_folder_path",
+    default=PREPARED_FINAL_DATA_FOLDER_PATH,
+    type=click.Path(),
+    help="Path to the folder where finally prepared data files are saved to",
+)
+def add_new_features(input_data_folder_path, prepared_final_data_folder_path):
+    prepared_final_data_folder_path.mkdir(parents=True, exist_ok=True)
+    train_data = pd.read_csv(Path(input_data_folder_path) / "train_data.csv")
+    test_data = pd.read_csv(Path(input_data_folder_path) / "test_data.csv")
+
+    train_data = add_features(train_data)
+    test_data = add_features(test_data)
+
+    train_data.to_csv(Path(prepared_final_data_folder_path) / "train_data.csv", index=False)
+    test_data.to_csv(Path(prepared_final_data_folder_path) / "test_data.csv", index=False)
+
+
+if __name__ == "__main__":
+    add_new_features()
--- src/store_sales/splitting_and_preparing_data.py
+++ src/store_sales/splitting_and_preparing_data.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from store_sales import (
     prepare_data,
     RAW_DATA_FOLDER_PATH,
-    PREPARED_DATA_STAGE_1_FOLDER_PATH,
+    PREPARED_FOR_EDA_DATA_FOLDER_PATH,
     NUMBER_OF_DAYS_TO_PREDICT
 )
 
@@ -17,13 +17,13 @@ from store_sales import (
     help="Path to the folder with raw data files",
 )
 @click.option(
-    "--prepared_data_folder_path",
-    default=PREPARED_DATA_STAGE_1_FOLDER_PATH,
+    "--prepared_for_eda_data_folder_path",
+    default=PREPARED_FOR_EDA_DATA_FOLDER_PATH,
     type=click.Path(),
-    help="Path to the folder where prepared files are saved to",
+    help="Path to the folder where prepared for EDA data files are saved to",
 )
-def split_and_prepare_data(raw_data_folder_path, prepared_data_folder_path):
-    prepared_data_folder_path.mkdir(parents=True, exist_ok=True)
+def split_and_prepare_data(raw_data_folder_path, prepared_for_eda_data_folder_path):
+    prepared_for_eda_data_folder_path.mkdir(parents=True, exist_ok=True)
     data = pd.read_csv(Path(raw_data_folder_path) / "train.csv")
     holidays_events_data = pd.read_csv(
         Path(raw_data_folder_path) / "holidays_events.csv"
@@ -37,8 +37,8 @@ def split_and_prepare_data(raw_data_folder_path, prepared_data_folder_path):
     train_data = data[pd.to_datetime(data["date"]) < min_test_date]
     test_data = data[pd.to_datetime(data["date"]) >= min_test_date]
 
-    train_data.to_csv(Path(prepared_data_folder_path) / "train_data.csv", index=False)
-    test_data.to_csv(Path(prepared_data_folder_path) / "test_data.csv", index=False)
+    train_data.to_csv(Path(prepared_for_eda_data_folder_path) / "train_data.csv", index=False)
+    test_data.to_csv(Path(prepared_for_eda_data_folder_path) / "test_data.csv", index=False)
 
 
 if __name__ == "__main__":
