17bbb865760a185593af2f32eff9a1d5ccd5cbb8
Stas <Stas20042701@gmail.com@>
2024-08-26 17:03:56 +0300

    Change all paths in constants.py from relative to absolute

--- src/store_sales/constants.py
+++ src/store_sales/constants.py
@@ -4,14 +4,14 @@ NOT_HOLIDAY_DAY = "Not holiday"
 NUMBER_OF_DAYS_TO_PREDICT = 15
 
 
-DATA_FOR_STREAMLIT_PATH = Path("../../data/prepared_data/train_data.csv")
+DATA_FOR_STREAMLIT_PATH = Path(__file__).resolve().parent.parent.parent / "data" / "prepared_data" / "train_data.csv"
 
-RAW_DATA_FOLDER_PATH = Path("../../data/raw_data")
-PREPARED_DATA_STAGE_1_FOLDER_PATH = Path("../../data/prepared_data")
-PREPARED_DATA_STAGE_2_FOLDER_PATH = Path("../../data/prepared_data")
+RAW_DATA_FOLDER_PATH = Path(__file__).resolve().parent.parent.parent / "data" / "raw_data"
+PREPARED_DATA_STAGE_1_FOLDER_PATH = Path(__file__).resolve().parent.parent.parent / "data" / "prepared_data"
+PREPARED_DATA_STAGE_2_FOLDER_PATH = Path(__file__).resolve().parent.parent.parent / "data" / "prepared_data"
 
-METRICS_FOLDER_PATH = Path("../../data/models_and_metrics")
-MODELS_FOLDER_PATH = Path("../../data/models_and_metrics")
+METRICS_FOLDER_PATH = Path(__file__).resolve().parent.parent.parent / "data" / "models_and_metrics"
+MODELS_FOLDER_PATH = Path(__file__).resolve().parent.parent.parent / "data" / "models_and_metrics"
 
 SERVER_URI = "http://127.0.0.1:8080"
 DEFAULT_EXPERIMENT_NAME = "Cross validation models experiment"
