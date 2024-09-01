7164681083ca3bb2195277e12442419d4beea778
Stas <Stas20042701@gmail.com@>
2024-09-01 19:06:16 +0300

    Create ROOT_PATH and DATA_PATH constants for a better paths organization

--- src/store_sales/modules/constants.py
+++ src/store_sales/modules/constants.py
@@ -22,35 +22,20 @@ OIL_PRICE_FALLING_FINISH_1 = pd.to_datetime("2015-01-31")
 OIL_PRICE_FALLING_START_2 = pd.to_datetime("2015-06-01")
 OIL_PRICE_FALLING_FINISH_2 = pd.to_datetime("2016-02-29")
 
-EPSILON = 10 ** -5
+EPSILON = 10**-5
 N_SPLITS = 5
 N_SHOPS_OPTUNA = 5
 N_TRIALS_OPTUNA = 100
 
+ROOT_PATH = Path(__file__).resolve().parent.parent.parent.parent
+DATA_PATH = ROOT_PATH / "data"
 
-DATA_FOR_STREAMLIT_PATH = (
-    Path(__file__).resolve().parent.parent.parent.parent
-    / "data"
-    / "prepared_data"
-    / "train_data.csv"
-)
-
-RAW_DATA_FOLDER_PATH = (
-    Path(__file__).resolve().parent.parent.parent.parent / "data" / "raw_data"
-)
-PREPARED_FOR_EDA_DATA_FOLDER_PATH = (
-    Path(__file__).resolve().parent.parent.parent.parent / "data" / "prepared_for_eda_data"
-)
-PREPARED_FINAL_DATA_FOLDER_PATH = (
-    Path(__file__).resolve().parent.parent.parent.parent / "data" / "prepared_final_data"
-)
-
-METRICS_FOLDER_PATH = (
-    Path(__file__).resolve().parent.parent.parent.parent / "data" / "models_and_metrics"
-)
-MODELS_FOLDER_PATH = (
-    Path(__file__).resolve().parent.parent.parent.parent / "data" / "models_and_metrics"
-)
+DATA_FOR_STREAMLIT_PATH = DATA_PATH / "prepared_for_eda_data" / "train_data.csv"
+RAW_DATA_FOLDER_PATH = DATA_PATH / "raw_data"
+PREPARED_FOR_EDA_DATA_FOLDER_PATH = DATA_PATH / "prepared_for_eda_data"
+PREPARED_FINAL_DATA_FOLDER_PATH = DATA_PATH / "prepared_final_data"
+METRICS_FOLDER_PATH = DATA_PATH / "models_and_metrics"
+MODELS_FOLDER_PATH = DATA_PATH / "models_and_metrics"
 
 DEFAULT_SERVER_URI = "http://127.0.0.1:8080"
 DEFAULT_EXPERIMENT_NAME = "Cross validation models experiment"
