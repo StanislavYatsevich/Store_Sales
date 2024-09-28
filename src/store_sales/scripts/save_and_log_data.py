1be9ebd071bf7be93115c3dc34022444d82068e9
Stas <Stas20042701@gmail.com@>
2024-09-28 23:38:50 +0300

    Change the model to LGBMRegressor and refactor the code to perform categorical data auto-handling

--- src/store_sales/scripts/save_and_log_data.py
+++ src/store_sales/scripts/save_and_log_data.py
@@ -7,12 +7,12 @@ from sklearn.model_selection import TimeSeriesSplit
 from sklearn.metrics import mean_absolute_error
 from sklearn.base import clone
 from pathlib import Path
+#import lightgbm as lgb
 from store_sales.modules import (
-    encode_features,
-    get_mae,
     get_models_and_metrics_cv_and_testing,
+    calculate_daily_metrics,
     save_metrics_and_models,
-    optimize_xgboost_params_with_optuna,
+    optimize_lgb_params_with_optuna,
     PREPARED_FINAL_DATA_FOLDER_PATH,
     METRICS_FOLDER_PATH,
     MODELS_FOLDER_PATH,
@@ -53,14 +53,31 @@ def save_and_log_data(metrics_folder_path, models_folder_path, send_to_server):
 
     metrics_cv_file = Path(metrics_folder_path) / "metrics_cv.json"
     metrics_test_file = Path(metrics_folder_path) / "metrics_test.json"
+    daily_metrics_file = Path(metrics_folder_path) / "daily_metrics_test.json"
+
     models_file = Path(models_folder_path) / "models.pkl"
 
     train_data = pd.read_csv(PREPARED_FINAL_DATA_FOLDER_PATH / "train_data.csv")
     test_data = pd.read_csv(PREPARED_FINAL_DATA_FOLDER_PATH / "test_data.csv")
 
-    optimized_model, best_params = optimize_xgboost_params_with_optuna(
+    optimized_model, best_params = optimize_lgb_params_with_optuna(
         train_data, N_SHOPS_OPTUNA, N_TRIALS_OPTUNA
     )
+
+    '''params_lgb = {
+        'max_depth': 2,
+        'n_estimators': 115,
+        'learning_rate': 0.02424128710744379,
+        'subsample': 0.6976019464448409,
+        'colsample_bytree': 0.9747490499129191,
+        'lambda_l1': 1.2591101610869463e-06,  
+        'lambda_l2': 9.766607974404143e-05,  
+        'min_split_gain': 0.2651803122030023,
+        'verbose' : -1,
+    }
+
+    optimized_model = lgb.LGBMRegressor(**params_lgb, random_state=42, n_jobs=-1)'''
+
     tscv = TimeSeriesSplit(n_splits=N_SPLITS)
 
     if not send_to_server:
@@ -71,6 +88,7 @@ def save_and_log_data(metrics_folder_path, models_folder_path, send_to_server):
             mae_scores_test,
             avg_sales_test,
             wmape_percentage_scores_test,
+            daily_metrics_test,
             models,
         ) = get_models_and_metrics_cv_and_testing(
             train_data, test_data, optimized_model
@@ -78,6 +96,7 @@ def save_and_log_data(metrics_folder_path, models_folder_path, send_to_server):
         save_metrics_and_models(
             metrics_cv_file,
             metrics_test_file,
+            daily_metrics_file,
             models_file,
             mae_scores_cv,
             avg_sales_cv,
@@ -85,6 +104,7 @@ def save_and_log_data(metrics_folder_path, models_folder_path, send_to_server):
             mae_scores_test,
             avg_sales_test,
             wmape_percentage_scores_test,
+            daily_metrics_test,
             models,
         )
     else:
@@ -98,34 +118,37 @@ def save_and_log_data(metrics_folder_path, models_folder_path, send_to_server):
         mae_scores_test = dict()
         avg_sales_test = dict()
         wmape_percentage_scores_test = dict()
+        daily_metrics_test = dict()
 
         models_fitted = dict()
+
+        cat_columns = [col for col in train_data.columns if train_data[col].dtype == 'object']
+        for col in cat_columns:
+            train_data[col] = train_data[col].astype('category')
+            test_data[col] = test_data[col].astype('category')
 
         for store_num in train_data["store_number"].unique():
             for item_family in train_data["item_family"].unique():
                 mae_scores_this_split = []
-                train_data = train_data[
+                data = train_data[
                     (train_data["store_number"] == store_num)
                     & (train_data["item_family"] == item_family)
                 ]
-                X = train_data.drop(["item_sales"], axis=1)
-                y = train_data["item_sales"]
+                X = data.drop(["item_sales"], axis=1)
+                y = data["item_sales"]
                 for train_index, test_index in tscv.split(X):
                     X_train, X_test = X.iloc[train_index], X.iloc[test_index]
                     y_train, y_test = y.iloc[train_index], y.iloc[test_index]
-                    X_train_encoded, X_test_encoded = encode_features(X_train, X_test)
                     model_clone_cv = clone(optimized_model)
-                    mae_cv = get_mae(
-                        X_train_encoded, X_test_encoded, y_train, y_test, model_clone_cv
-                    )
+
+                    model_clone_cv.fit(X_train, y_train, categorical_feature=cat_columns)
+                    y_pred = pd.Series(model_clone_cv.predict(X_test), index=y_test.index)
+                    mae_cv = mean_absolute_error(y_test, y_pred)
+
                     mae_scores_this_split.append(mae_cv)
 
-                avg_sales_this_series_cv = np.round(
-                    np.mean(train_data["item_sales"]), 2
-                )
-                mae_this_series_cv = np.round(
-                    np.mean(np.array(mae_scores_this_split)), 2
-                )
+                avg_sales_this_series_cv = np.round(np.mean(data["item_sales"]), 2)
+                mae_this_series_cv = np.round(np.mean(np.array(mae_scores_this_split)), 2)
                 wmape_percentage_this_series_cv = np.round(
                     100 * mae_this_series_cv / (avg_sales_this_series_cv + EPSILON), 2
                 )
@@ -136,25 +159,22 @@ def save_and_log_data(metrics_folder_path, models_folder_path, send_to_server):
                     wmape_percentage_this_series_cv
                 )
 
-                test_data = test_data[
+                data = test_data[
                     (test_data["store_number"] == store_num)
                     & (test_data["item_family"] == item_family)
                 ]
-                X_train = train_data.drop(["item_sales"], axis=1)
-                y_train = train_data["item_sales"]
-                X_test = test_data.drop(["item_sales"], axis=1)
-                y_test = test_data["item_sales"]
-                X_train_encoded, X_test_encoded = encode_features(X_train, X_test)
+                X_train = X
+                y_train = y
+                X_test = data.drop(["item_sales"], axis=1)
+                y_test = data["item_sales"]
                 model_clone_test = clone(optimized_model)
 
-                model_clone_test.fit(X_train_encoded, y_train)
-                y_pred = pd.Series(
-                    model_clone_test.predict(X_test_encoded), index=y_test.index
-                )
+                model_clone_test.fit(X_train, y_train, categorical_feature=cat_columns)
+                y_pred = pd.Series(model_clone_test.predict(X_test), index=y_test.index)
 
                 mae_this_series_test = np.round(mean_absolute_error(y_test, y_pred), 2)
                 avg_sales_this_series_test = np.round(
-                    np.mean(test_data["item_sales"]), 2
+                    np.mean(data["item_sales"]), 2
                 )
                 wmape_percentage_this_series_test = np.round(
                     100 * mae_this_series_test / (avg_sales_this_series_test + EPSILON),
@@ -167,6 +187,8 @@ def save_and_log_data(metrics_folder_path, models_folder_path, send_to_server):
                     wmape_percentage_this_series_test
                 )
 
+                daily_metrics_test[(store_num, item_family)] = calculate_daily_metrics(y_test, y_pred)
+
                 models_fitted[(store_num, item_family)] = model_clone_test
 
                 metrics = {
@@ -176,6 +198,7 @@ def save_and_log_data(metrics_folder_path, models_folder_path, send_to_server):
                     "MAE Test": mae_this_series_test,
                     "Mean sales Test": avg_sales_this_series_test,
                     "WMAPE in percentage Test": wmape_percentage_scores_test,
+                    "Daily metrics Test" : daily_metrics_test,
                 }
                 tags = {
                     "Model": "XGBRegressor",
