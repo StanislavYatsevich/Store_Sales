f944094e6c0115c1508615c3fd78956b3d3f76a3
Stas <Stas20042701@gmail.com@>
2024-09-29 16:12:54 +0300

    Change the model used to LightGBM and refactor the code according to it

--- src/store_sales/modules/hyperparameters_optimization.py
+++ src/store_sales/modules/hyperparameters_optimization.py
@@ -1,29 +1,31 @@
 import numpy as np
 import pandas as pd
 import optuna
-import xgboost as xgb
+import lightgbm as lgb
 from typing import Tuple
 from sklearn.model_selection import TimeSeriesSplit
-from store_sales.modules import encode_features, get_mae, N_SPLITS
+from sklearn.metrics import mean_absolute_error
+from sklearn.base import clone
+from store_sales.modules import N_SPLITS
 
 
-def optimize_xgboost_params_with_optuna(
+def optimize_lgb_params_with_optuna(
     train_data: pd.DataFrame, shops_number: int, n_trials: int
-) -> Tuple[xgb.XGBRegressor, dict]:
-    """Optimizes hyperparameters of a given XGBRegressor.
+) -> Tuple[lgb.LGBMRegressor, dict]:
+    """Optimizes hyperparameters of a given LGBMRegressor.
 
-    Finds optimal hyperparameters for an XGBRegressor within set boundaries
+    Finds optimal hyperparameters for an LGBMRegressor within set boundaries
     for a given number of shops and trials. The optimization criteria is
     Mean Absolute Error (MAE) calculated using the cross-validation technique.
 
     Args:
         train_data: pd.DataFrame instance representing the train part of the data.
-        shops_number: number of shops (<= 54) to be randomly chosen for finding
-            optimal hyperparameters.
+        shops_number: number of shops (<= 54 since there are exactly 54 shops in the dataset)
+        to be randomly chosen for finding optimal hyperparameters.
         n_trials: number of trials for finding optimal hyperparameters.
 
     Returns:
-        A Tuple(best_model, best_params) where best_params is the dictionary
+        Tuple(best_model, best_params) where best_params is the dictionary
         representing the hyperparameters grid and best_model is the optimal
         model.
     """
@@ -32,6 +34,10 @@ def optimize_xgboost_params_with_optuna(
         train_data["store_number"].unique(), shops_number, replace=False
     )
 
+    cat_columns = [col for col in train_data.columns if train_data[col].dtype == 'object']
+    for col in cat_columns:
+        train_data[col] = train_data[col].astype('category')
+
     def objective(trial) -> float:
         parameters = {
             "max_depth": trial.suggest_int("max_depth", 2, 10),
@@ -39,14 +45,15 @@ def optimize_xgboost_params_with_optuna(
             "learning_rate": trial.suggest_float("learning_rate", 0.01, 0.3),
             "subsample": trial.suggest_float("subsample", 0.5, 1.0),
             "colsample_bytree": trial.suggest_float("colsample_bytree", 0.5, 1.0),
-            "gamma": trial.suggest_float("gamma", 0, 0.5),
-            "lambda": trial.suggest_float("lambda", 1e-8, 1.0, log=True),
-            "alpha": trial.suggest_float("alpha", 1e-8, 1.0, log=True),
+            "min_split_gain": trial.suggest_float("min_split_gain", 0, 0.5),
+            "lambda_l2": trial.suggest_float("lambda_l2", 1e-8, 1.0, log=True),
+            "lambda_l1": trial.suggest_float("lambda_l1", 1e-8, 1.0, log=True),
         }
-        model = xgb.XGBRegressor(**parameters, random_state=42, n_jobs=-1)
+        model = lgb.LGBMRegressor(**parameters, random_state=42, n_jobs=-1, verbose=-1)
         mae_scores = []
         for store_num in random_stores:
             for item_family in train_data["item_family"].unique():
+                mae_scores_this_split = []
                 data = train_data[
                     (train_data["store_number"] == store_num)
                     & (train_data["item_family"] == item_family)
@@ -56,11 +63,11 @@ def optimize_xgboost_params_with_optuna(
                 for train_index, test_index in tscv.split(X):
                     X_train, X_test = X.iloc[train_index], X.iloc[test_index]
                     y_train, y_test = y.iloc[train_index], y.iloc[test_index]
-                    X_train_encoded, X_test_encoded = encode_features(X_train, X_test)
-                    mae = get_mae(
-                        X_train_encoded, X_test_encoded, y_train, y_test, model
-                    )
-                    mae_scores.append(mae)
+                    model_clone = clone(model)
+                    model_clone.fit(X_train, y_train, categorical_feature=cat_columns)
+                    y_pred = pd.Series(model_clone.predict(X_test), index=y_test.index)
+                    mae_cv = mean_absolute_error(y_test, y_pred)
+                    mae_scores_this_split.append(mae_cv)
 
         return np.mean(np.array(mae_scores))
 
@@ -68,6 +75,6 @@ def optimize_xgboost_params_with_optuna(
     study.optimize(objective, n_trials=n_trials)
 
     best_params = study.best_params
-    best_model = xgb.XGBRegressor(**best_params, random_state=42, n_jobs=-1)
+    best_model = lgb.LGBMRegressor(**best_params, random_state=42, n_jobs=-1, verbose=-1)
 
     return best_model, best_params
